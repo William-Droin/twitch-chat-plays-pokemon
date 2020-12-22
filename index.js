9a01e372abfa2c10d093ab57675118fc7be056ba
Kazim-C <72973649+Kazim-C@users.noreply.github.com>
2020-12-22 16:32:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -109,26 +109,6 @@ client.on('message', (channel, tags, message, self) => {
 
 
 
-if(message.toLowerCase() === 'mousetest'){
-
-		// Move the mouse across the screen as a sine wave.
-
-
-		// Speed up the mouse.
-		robot.setMouseDelay(2);
-
-		var twoPI = Math.PI * 2.0;
-		var screenSize = robot.getScreenSize();
-		var height = (screenSize.height / 2) - 10;
-		var width = screenSize.width;
-
-		for (var x = 0; x < width; x++)
-		{
-			y = height * Math.sin((twoPI * x) / width) + height;
-			robot.moveMouse(x, y);
-		}
-}
-
 
 //Keyboard script
 
